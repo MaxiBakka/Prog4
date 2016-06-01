c66abf5724fbeb219524c5b1636fff7bf76ecfd2
MaxiBakka <maxi.bakka@gmail.com>
2016-06-01 14:03:47 -0300

    Agregue operaciones de la Clase interesado
    arregle errores(sintaxis,etc), agregue operaciones

--- Interesado.h
+++ Interesado.h
@@ -1,34 +1,43 @@
 #ifndef Interesado.h
 #define Interesado.h
 
-#include Usuario.h
-#include Chat.h
+#include "Usuario.h"
+#include "Chat.h"
 
-class Interesado : public Usuario:
+#include <set>
+#include <string>
+
+class DtInteresado;
+class DataMensaje;
+
+class Interesado : public Usuario{
     private:
         string nombre;
         string apellido;
         int edad;
-        //Set(Chat) chats;
+        set<Chat*>*chats;
     public:
         Interesado();
-        Interesado(string e,string c,string n,string a,int age);
-        virtual string get_email();
-        virtual string get_contrasenia();
-        virtual void set_email(string &e);
-        virtual void set_contrasenia(string &c);
-        string get_nombre();
-        string get_apellido();
+        Interesado(const DataInteresado*di);
+        Interesado(string& e,string& c,string& n,string& a,int age);
+        //getters y setters
+        string& get_nombre();
+        string& get_apellido();
         int get_edad();
         void set_nombre(string &n);
         void set_apellido(string &a);
         void set_edad(int &age);
-        //ObtenerDataMensajes():set(DataMensaje);
-        //AgregarChat(Chat *chat);
-        //EliminarChat(Chat *chat);
-        //notify(Mail notificacionMail,int codigo);
-        virtual ~Usuario;
+        
+        //obtencion datatype
+        set<DataMensaje*>* ObtenerDataMensajes();
+        //operaciones de chat
+        void AgregarChat(Chat *chat);
+        void EliminarChat(Chat *chat);
+        bool ExisteChat(const string& email);
+       
+        
+        virtual ~Interesado();
 
-
+};
 
 #endif // Interesado
