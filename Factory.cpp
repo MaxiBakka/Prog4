4ec779368a514dd6ef2af33ba7c66bc1a896cbef
MaxiBakka <maxi.bakka@gmail.com>
2016-06-01 00:04:41 -0300

    Corregi un par de cosas

--- Factory.cpp
+++ Factory.cpp
@@ -1,20 +1,23 @@
-#include "Facotry.h"
+#include "Factory.h"
 
-class Factory {
-	
+
 	Factory* Factory::instancia = NULL;
-	Factory* Factory(){}
+	
+	Factory::Factory(){};
+	
+	Factory::~Factory(){};
+	
 	Factory* Factory::getInstance(){
 		if (instancia==NULL)
 			instancia = new Factory();		
 		return instancia;
 	}
 
-	IPropiedadController Factory::getIPropiedadController(){
+	IPropiedadController *Factory::getIPropiedadController(){
 		return new PropiedadController();
 	}
 
-	IUsuarioController Factory::getIUsuarioController(){
+	IUsuarioController *Factory::getIUsuarioController(){
 		return new UsuarioController();
 	}
-}
+
