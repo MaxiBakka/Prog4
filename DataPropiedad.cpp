bd291717e41e38859b65d872b3ba9349a6fc6c10
MaxiBakka <maxi.bakka@gmail.com>
2016-05-31 23:09:56 -0300

    Implementar metodo de ostream

--- DataPropiedad.cpp
+++ DataPropiedad.cpp
@@ -1,15 +1,10 @@
 #include "DataPropiedad.h"
 
 
-/*DataPropiedad :: DataPropiedad(int cantAmbientes,int dorm,int banios,string direccion,bool garaje,DataInfoAlquiler *&alquiler,DataInfoVenta*&venta){
-	this->cantAmbientes=cantAmbientes;
-	this->dormitorios=dorm;
-	this->banios=banios;
-	this->direccion=direccion;
-	this->garaje=garaje;
-	this->info_alquiler=alquiler;
-	this->info_venta=venta;
-
-}*/
+ostream & operator <<(ostream &output, DataPropiedad* dp){
+	
+	
+	
+}
 
 
