3eee5bc3dc10784669068139b452c20c1c16e1a4
MaxiBakka <maxi.bakka@gmail.com>
2016-06-01 13:43:17 -0300

    quite los getters y setters implementados en Usuario

--- Administrador.cpp
+++ Administrador.cpp
@@ -1,28 +1,13 @@
 #include Administrador.h
 
-Administrador :: Administrador(){
-    Usuario();
+Administrador :: Administrador():Usuario(){
+    
 }
 
-Administrador :: Administrador (string e,string c){
-    Usuario(e,c);
-}
-
-string Administrador :: get_email(){
-    return this->email;
-}
-
-string Administrador :: get_contraseña(){
-    return this->email;
-}
-
-void Administrador :: set_email(string &e){
-    this->email=e;
-}
-
-void Aministrador :: set_contraseña(string &c){
-    this->contraseña=c;
+Administrador :: Administrador (string e,string c):Usuario(e,c){
+    
 }
 
 Administrador :: ~Administrador(){
+    
 }
