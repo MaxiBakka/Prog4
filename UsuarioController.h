cf1f4c0c429aac672a4b4be93bda7f437257f904
matutiano <matu23@gmail.com>
2016-06-01 20:51:35 -0300

    Create UsuarioController.h
    puse email y pwd xq no me acuerdo muy bien como ibamos a modelar la sesion, tampoco tengo muy claro si son estaticos o no.
    Queda en review.

---
+++ UsuarioController.h
@@ -0,0 +1,34 @@
+#ifndef UsuarioController.h
+#define UsuarioController.h
+
+#include "Manejador_Usuario.h"
+#include "Usuario.h"
+#include "IUsuarioController.h"
+#include "DataInmobiliaria.h"
+#include "DataInfoInmobiliaria.h"
+
+#include <set>
+#include <string>
+ 
+class UsuarioController {
+
+private:
+
+	//No se si va pero por las dudas lo dejo por aca
+	string* email;
+	string* password;
+
+public:
+
+	virtual void activarSesion();
+	virtual void cancelarInicioSesion();
+	virtual void confirmarContrasena(string pwd);
+	virtual bool esUsuarioAdmin(const string& email);
+	virtual void ingresarContrasena(string pwd);
+	virtual void ingresarContrasenaNueva(string pwd);
+	virtual bool primeraVez();
+	virtual void ingresarInmobiliaria(DataInmobiliaria* I);
+	virtual <DataInfoInmobiliaria*>* obtenerReporte();
+
+
+}
