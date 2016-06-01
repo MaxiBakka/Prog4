be2cbc5fa75f27962bd2374b03803660896737b6
MaxiBakka <maxi.bakka@gmail.com>
2016-06-01 13:41:23 -0300

    Update Administrador.h

--- Administrador.h
+++ Administrador.h
@@ -1,18 +1,14 @@
 #ifndef Administrador_h
 #define Administrador_h
 
-#include Usuario.h
+#include "Usuario.h"
 
-class Administrador : public Usuario:
-    private:
+class Administrador : public Usuario{
     public:
         Administrador();
-        Administrador(string e,string c);
-        virtual string get_email();
-        virtual string get_contraseña();
-        virtual void set_email(string &e);
-        virtual void set_contraseña(string &c);
-        virtual ~Administrador;
+        Administrador(string& e,string& c);
+        virtual ~Administrador();
+};
 
 
 #endif // Administrador_h
