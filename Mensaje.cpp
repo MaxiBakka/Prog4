b60e4fff4279d8c06a06fcd85a2e39c0d7610675
MaxiBakka <maxi.bakka@gmail.com>
2016-06-03 15:58:13 -0300

    Agregue los setters que faltaban

--- Mensaje.cpp
+++ Mensaje.cpp
@@ -1,25 +1,40 @@
 #include "Mensaje.h"
 
-Mensaje :: Mensaje(Fecha f, Hora h, string t){
+Mensaje :: Mensaje(Fecha& f, Hora& h, string& t){
     this->fecha=f;
     this->hora=h;
     this->texto=t;
 }
 
-Fecha Mensaje :: getFecha(){
+Fecha& Mensaje :: getFecha(){
     return this->fecha;
 }
 
-Hora Mensaje :: getHora(){
+Hora& Mensaje :: getHora(){
     return this->hora;
 }
 
-string Mensaje :: getTexto(){
+string& Mensaje :: getTexto(){
     return this->texto;
 }
 
-DataMensaje Mensaje :: getDataMensaje(){
-    return DataMensaje(this->fecha,this->hora,this->texto);
+//setters
+
+void Mensaje::setTexto(string& texto){
+    this->texto=texto;
+    
+}
+
+void Mensaje::setFecha(Fecha&fecha){
+    
+    this->fecha=fecha;
+}
+void Mensaje::setHora(Hora& hora){
+    this->hora=hora;
+}
+
+DataMensaje* Mensaje :: getDataMensaje(){
+    return new DataMensaje(this->fecha,this->hora,this->texto);
 }
 
 Mensaje :: ~Mensaje(){
