fa3dfc95eca6c3aa525df5cd4cb452130344e158
Maximiliano Bakkalian <maxi.bakka@gmail.com>
2016-06-04 01:43:59 -0300

    Agregue las asociaciones de interesado,propiedad,y inmobiliaria

--- Chat.cpp
+++ Chat.cpp
@@ -1,9 +1,15 @@
 #include "Chat.h"
 
-Chat :: Chat(string ei,string ni){
+using namespace std;
+
+
+Chat::Chat(string ei,string ni,Propiedad*p,Interesado*i,Inmobiliaria*inm{
     this->emailInteresado=ei;
     this->nombreInmobiliaria=ni;
-    std::stack<Mensaje*>* this->mensajes;
+    this->mensajes= new stack<Mensaje*>()
+    this->propiedad=p;
+    this->interesado=i;
+    this->inmobiliaria=inm;
 }
 
 string Chat :: getEmailInteresado(){
@@ -18,28 +24,46 @@ std::stack<Mensaje*>* Chat::getMensajes() {
     //Despues hay que hacerlo
 }
 
+Propiedad* Chat::getPropiedad(){
+	
+}
+
+Interesado* Chat::getInteresado(){
+	
+	
+}
+Inmobiliaria *Chat::getInmobiliaria(){
+	
+	
+}
+void Chat:: setEmailInteresado(string email){
+	
+	
+}
+void Chat:: setNombreInmobiliaria(string nombre){
+	
+	
+}
+
+
 int Chat::cantidadMensajes() {
-    return mensajes.size();
+    return mensajes->size();
 }
 
 bool Chat::esChatPropiedad(int codigo) {
     //No se que hace esta operacion , no encontre ni el dss ni nada
 }
 
-std::stack<DataMensaje*>* Chat::getDataMensajes() {     //Devuelve los ultimos 6?
-    std::stack<Mensaje*>* aux (this->mensajes);
-    std::stack<DataMensaje*>* res;
-    for(int i=0;i<5;i++){
-        res.push(new DataMensaje(aux.top()->getFecha(),aux.top()->getHora(),aux.top()->getTexto()));
-    }
+vector<DataMensaje*>* Chat::getDataMensajes() {     //Devuelve los ultimos 6? Repuesta:5
+    
 }
 
 void Chat::nuevoMensaje(DataMensaje m) {
-    this->mensajes.push(new Mensaje(m->get_fecha(),m->get_hora(),m->get_texto()));
+    this->mensajes->pushback(new Mensaje(m->get_fecha(),m->get_hora(),m->get_texto()));
 }
 
 Chat::~Chat() {
-    while (!this->mensajes.empty()){
+    while (!this->mensajes->empty()){
         this->mensajes->pop();
     }
 }
--- Chat.h
+++ Chat.h
@@ -3,26 +3,52 @@
 #define CHAT_H
 
 #include<string>
-#include<stack>
+#include<vector>
+
 #include "Mensaje.h"
 #include "DataMensaje.h"
 
 
+
+using namespace std;
+
+class Interesado;
+class Inmobiliaria;
+class Propiedad;
+
+		
 class Chat {
 private:
     string emailInteresado;
     string nombreInmobiliaria;
-    std::stack<Mensaje*>* mensajes;
+    vector<Mensaje*>* mensajes;
+    Propiedad*propiedad;
+    Interesado*interesado;
+    Inmobiliaria * inmobiliaria;
+    
 public:
-    Chat(string ei,string ni);
+    Chat(string ei,string ni,Propiedad*p,Interesado*i,Inmobiliaria*inm);
+    //getters
     string getEmailInteresado();
     string getNombreInmobiliaria();
-    std::stack<Mensaje*>* getMensajes();
+    Propiedad* getPropiedad();
+    Interesado* getInteresado();
+    Inmobiliaria * getInmobiliaria();
+    //setters
+    void setEmailInteresado(string email);
+    void setNombreInmobiliaria(string nombre);
+    
+    //operaciones de caso de uso EnviarMensaje
     int cantidadMensajes();
     bool esChatPropiedad(int codigo);
-    std::stack<DataMensaje*>* getDataMensajes();
-    void nuevoMensaje(DataMensaje m);
-    ~Chat();
+    void nuevoMensaje(DataMensaje* mensaje);
+    //obtencion de datatype
+    vector<DataMensaje*>* getDataMensajes();
+    
+    //operaciones de sobre mensajes
+    
+    //destructor
+    virtual ~Chat();
 };
 
 
