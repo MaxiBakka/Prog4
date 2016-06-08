50dae9bf53f25d0e92943a31dd8516a21680c0fb
Matias <Matias@SystemM>
2016-06-08 03:19:00 -0300

    Revisar el comentario respecto a DataInombiliaria e Inmobiliaria

--- Manejador_Usuario.cpp
+++ Manejador_Usuario.cpp
@@ -2,12 +2,13 @@
 #include "InmobliariaNotFound.h"
 
 Manejador_Usuario* Manejador_Usuario::instancia = NULL;
-map<string,Administrador*>* Manejador_Usuario::administradores = NULL;
-map<string,Interesado*>* Manejador_Usuario::interesados = NULL;
-map<string,Inmobiliaria*>* Manejador_Usuario::inmobiliarias = NULL;
 
 
-Manejador_Usuario :: Manejador_Usuario(){};
+Manejador_Usuario :: Manejador_Usuario(){
+	inmobiliarias = new map<string,Inmobiliaria*>;
+	interesados =  new map<string,Interesado*>;
+	administradores = new map<string,Administrador*>;
+};
 Manejador_Usuario :: ~Manejador_Usuario(){};
 
 public Manejador_Usuario::getInstance(){
@@ -18,20 +19,44 @@ public Manejador_Usuario::getInstance(){
 //getters de usuarios
 Inmobiliaria* Manejador_Usuario::getInmobiliaria(string &email){
 	map<string,Inmobiliaria*>::iterator it;
-	it = inmobiliarias.find(email);
-	if(it != inmobiliarias.end()){
-		return inmobiliarias[email];
+	it = inmobiliarias->find(email);
+	if(it != inmobiliarias->end()){
+		return it->second();
 	}else{
 		throw InmobiliariaNotFound();
 	}
 
 }
-Interesado* Manejador_Usuario::getInteresado(string &email);
-Usuario* Manejador_Usuario::getUsuario(string &email);
+
+Interesado* Manejador_Usuario::getInteresado(string &email){
+	map<string,Interesado*>::iterator it;
+	it = interesados->find(email);
+	if(it != interesados->end()){
+		return it->second();
+	}else{
+		throw InteresadoNotFound();
+	}
+
+}
+Usuario* Manejador_Usuario::getUsuario(string &email){
+	map<string,Inmobiliaria*>::iterator it;
+	it = inmobiliarias->find(email);
+	if(it != inmobiliarias->end()){
+		return it->second();
+	}else{
+		throw UsuarioNotFound();
+	}
+}
 
 //operaciones del caso AltaInmobiliaria Y AltaInteresado
-void Manejador_Usuario::CrearInmobiliaria(DataInmobiliaria*di);
-void Manejador_Usuario::CrearInteresado(DataInteresado*di);
+void Manejador_Usuario::CrearInmobiliaria(DataInmobiliaria*di){
+	Inmobiliaria* inmo = new Inmobiliaria(di->get_email,di->get_nombre(),di->get_direccion()); //no le falta pwd a esto?? vichar DataInmo
+	inmobiliarias->insert(pair<string,Inmobiliaria*>(di->get_email(),inmo));
+}
+void Manejador_Usuario::CrearInteresado(DataInteresado*di){
+	Intresado * inter = new Interesado(di->get_email,di->get_nombre(),di->get_apellido(),di->get_edad());
+	interesados->insert(pair<string,Interesado*>(di->get_email(),inter));
+}
 
 //existencia del usuario
 bool Manejador_Usuario::existeUsuario(string& email);
