3b019e6c3a3f7c156d2a130b0b3e3191e3ec2914
MaxiBakka <maxi.bakka@gmail.com>
2016-06-08 12:25:36 -0300

    Deje un par de comentarios y aregue alguna cosa
    El password tiene que ir por defecto ,se lo agregue, faltaria arreglar la operacion getUsuario que deje unos comentarios

--- Manejador_Usuario.cpp
+++ Manejador_Usuario.cpp
@@ -1,15 +1,22 @@
 #include "Manejador_Usuario.h"
 #include "InmobliariaNotFound.h"
+#include "InteresadoNotFound.h"
+#include "UsuarioNotFound.h"
+
+#include <utility>
 
 Manejador_Usuario* Manejador_Usuario::instancia = NULL;
 
 
 Manejador_Usuario :: Manejador_Usuario(){
-	inmobiliarias = new map<string,Inmobiliaria*>;
-	interesados =  new map<string,Interesado*>;
-	administradores = new map<string,Administrador*>;
+	inmobiliarias = new map<string,Inmobiliaria*>();
+	interesados =  new map<string,Interesado*>();
+	administradores = new map<string,Administrador*>();
 };
-Manejador_Usuario :: ~Manejador_Usuario(){};
+Manejador_Usuario :: ~Manejador_Usuario(){
+	
+	//creo que aca deberia destruirse todos los usuarios, o destruir la coleccion
+};
 
 public Manejador_Usuario::getInstance(){
 	if (instancia==NULL) instancia = new Manejador_Usuario();
@@ -38,8 +45,8 @@ Interesado* Manejador_Usuario::getInteresado(string &email){
 	}
 
 }
-Usuario* Manejador_Usuario::getUsuario(string &email){
-	map<string,Inmobiliaria*>::iterator it;
+Usuario* Manejador_Usuario::getUsuario(string &email){   // esta operacion se tiene que fijar en todas las colecciones para ver que tipo
+	map<string,Inmobiliaria*>::iterator it;		//de usuario retornar
 	it = inmobiliarias->find(email);
 	if(it != inmobiliarias->end()){
 		return it->second();
@@ -50,11 +57,11 @@ Usuario* Manejador_Usuario::getUsuario(string &email){
 
 //operaciones del caso AltaInmobiliaria Y AltaInteresado
 void Manejador_Usuario::CrearInmobiliaria(DataInmobiliaria*di){
-	Inmobiliaria* inmo = new Inmobiliaria(di->get_email,di->get_nombre(),di->get_direccion()); //no le falta pwd a esto?? vichar DataInmo
-	inmobiliarias->insert(pair<string,Inmobiliaria*>(di->get_email(),inmo));
-}
+	Inmobiliaria* inmo = new Inmobiliaria(di->get_email()," ",di->get_nombre(),di->get_direccion()); //no le falta pwd a esto?? vichar DataInmo
+	inmobiliarias->insert(pair<string,Inmobiliaria*>(di->get_email(),inmo));	//respuesta:aca es cuando el administrador crea un usuario 
+}											//y le asigna un pwd por defecto 
 void Manejador_Usuario::CrearInteresado(DataInteresado*di){
-	Intresado * inter = new Interesado(di->get_email,di->get_nombre(),di->get_apellido(),di->get_edad());
+	Intresado * inter = new Interesado(di->get_email()," ",di->get_nombre(),di->get_apellido(),di->get_edad());
 	interesados->insert(pair<string,Interesado*>(di->get_email(),inter));
 }
 
