7cccf7538a6a8cb9452983eeda59266f84090b48
gianniFing <gianni.testa@UY093NT004.uy.infra.d>
2016-06-08 21:49:13 -0300

    Agrego las operaciones de el caso de uso Consultar Propiedad.

---
+++ laboratorio6/PropiedadController.cpp
@@ -0,0 +1,13 @@
+#include "PropiedadController.h"
+
+PropiedadController::PropiedadController() {
+    this->zona=NULL;
+    this->da=NULL;
+    this->dv=NULL;
+    this->propiedad=NULL;
+}
+
+void PropiedadController::confirmarAltaPropiedad() {
+
+}
+
