befc3cb328ef2d8e19cd47a60d7b7eb66034edf9
Matias <Matias@SystemM.guest.fing.edu.uy>
2016-06-15 22:14:23 -0300

    se agrega carpeta rutinas

---
+++ Rutinas/RutinaInicioSesion.h
@@ -0,0 +1,14 @@
+
+#include "UserInterface.h"
+
+#ifndef RUTINA_INCIO_SESION_H_
+#define RUTINA_INCIO_SESION_H_
+
+class RutinaInicioSesion: public UserIterface {
+public:
+	RutinaInicioSesion();
+	void ejecutar();
+	virtual ~RutinaInicioSesion();
+};
+
+#endif /* RUTINA_INCIO_SESION_H_*/
