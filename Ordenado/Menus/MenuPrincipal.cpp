ceedeb7a922a322652144145012c8d90b0f65429
Maximiliano Bakkalian <maxi.bakka@gmail.com>
2016-06-17 00:52:09 -0300

    Arreglando menu principal

--- Ordenado/Menus/MenuPrincipal.cpp
+++ Ordenado/Menus/MenuPrincipal.cpp
@@ -2,13 +2,11 @@
 
 #include "MenuPrincipal.h"
 #include "MenuInteresados.h"
-#include "MenuInmobiliarias.h
-#include "MenuAdministradores.h
+#include "MenuInmobiliarias.h"
+#include "MenuAdministradores.h"
 
 #include "RutinaInicioSesion.h"
-#include "RutinaAltaInmobiliaria.h"
-#include "RutinaAltaInteresado.h"
-#include "RutinaCargarDatos.h"
+
 
 #include "Sesion.h"
 #include "Interesado.h"
@@ -28,10 +26,8 @@ MenuPrincipal::MenuPrincipal() {
 }
 
 void MenuPrincipal::printMenu(){
-	cout << "Menu principal" << endl;
+	cout << "Menu principal de Mi Casa" << endl;
 	cout << "1 - Iniciar Sesion" << endl;
-	cout << "2 - Cargar Set de Datos de Prueba" << endl;
-	cout << "3 - Cerrar Sesion"<<  endl;
 	cout << endl;
 	cout << "0 - Salir" << endl;
 }
@@ -45,12 +41,6 @@ void MenuPrincipal::ejecutarOpcion(int opc){
 	case 1:
 		submenu = new RutinaInicioSesion();
 		break;
-	case 2:
-		submenu = new RutinaCargarDatos();
-		break;
-	case 3:
-
-
 	default:
 		throw ExOpcionInvalida();
 	}
@@ -62,14 +52,18 @@ void MenuPrincipal::ejecutarOpcion(int opc){
 		Usuario*user=sesion->getUsuario();
 		if(Interesado*interesado=dynamic_cast<Interesado*>(user)){
 			submenu= new MenuIntersados();
+			submenu->ejecutar;
+			delete submenu;
 		}
 		else if(Inmobiliaria*inm=dynamic_cast<Inmobiliaria*>(user)){
 			submenu= new MenuInmobiliarias();
+			submenu->ejecutar;
+			delete submenu;
 		}
 		else if (Administrador*adm=dynamic_cast<Administrador*>(user)){
 			submenu= new MenuAdministradores();
-		}else{
-			break;
+			submenu->ejecutar;
+			delete submenu;
 		}
 	}
 }
