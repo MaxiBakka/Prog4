b2eddbfb0c93c122236c991e7c6653b2ac36d389
Matias <Matias@SystemM>
2016-06-17 03:20:48 -0300

    Se agrega rutina de modificar propiedad

---
+++ Ordenado/Rutinas/RutinaModificarPropiedad.h
@@ -0,0 +1,23 @@
+#include "../../UserInterface.h"
+#include "IModificarPropiedadController.h"
+
+class DataPropiedad;
+class IPropiedadController;
+
+#ifndef RUTINA_MODIFICAR_PROPIEDAD_H_
+#define RUTINA_MODIFICAR_PROPIEDAD_H_
+
+class RutinaModificarPropiedad: public PantallaUI {
+private:
+	IModificarPropiedadController* ctrl;
+	DataPropiedad* propiedad;
+	void seleccionarPropiedad();
+	void modificarDatos();
+
+public:
+	RutinaModificarPropiedad();
+	void ejecutar();
+	virtual ~RutinaModificarPropiedad();
+};
+
+#endif /* RUTINA_MODIFICAR_PROPIEDAD */
